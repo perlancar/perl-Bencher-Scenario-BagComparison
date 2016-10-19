9993f9a1e3434c81f1391b97b3194ca22d906f65
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-19 17:56:45 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/BagComparison.pm
@@ -0,0 +1,39 @@
+package Bencher::Scenario::BagComparison;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark bag comparison',
+    participants => [
+        {
+            module => 'Test::Deep', # Test::Deep::NoTest is useless without importing
+            code_template=>'state $bag = Test::Deep::bag(@{<bag>}); Test::Deep::eq_deeply(<array>, $bag)',
+        },
+    ],
+    datasets => [
+        {name=>'elems=10num' , args=>{array=>[1..10] , bag=>[reverse 1..10] }},
+        {name=>'elems=100num', args=>{array=>[1..100], bag=>[reverse 1..100]}},
+        {name=>'elems=200num', args=>{array=>[1..200], bag=>[reverse 1..200]}},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+TODO: find another bag comparison module.
+
+TODO: compare complex elements.
+
+TODO: compare with Data::Compare + sorting.
+
+
+=head1 SEE ALSO
+
+L<Bencher::Scenario::HashBuilding>
